09c52ab5b8e3adba1cf5b3e3d1f4cc79da8edfd4
lazorfuzz <leontosy@gmail.com>
2019-06-05 14:56:07 -0700

    instructor recommendations

--- src/lab3team3/Lab3Team3.java
+++ src/lab3team3/Lab3Team3.java
@@ -9,34 +9,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.stream.Collectors;
-import java.lang.Object.*;
-import org.json.simple.JSONArray;
-import org.json.simple.*;
 import com.google.gson.*;
-import org.json.simple.JSONObject;
 
-/*Looks good but I would also check the payload returned to make there is no error message and is well-formed. 
+/* 
+Project: L03: Group Work - Connections
+Purpose Details: Fetch data from the NOAA
+Course: IST 411
+Author: Team 3
+Date Developed: 2 June 2019
+Last Date Changed: 5 June 2019
+Revision: Added Oakes' recommendations
+*/
 
-Can you try and make a small change to the payload parsing to perform the check. 
-
-Also please include a last general exception check catch for any other exceptions that might fall through. Rework and respond by Wednesday.
-
-
-} catch (Exception e) { // General Exception for all Excpetions that fall through } finally { // be sure to clean up; close both the input stream and the socket*/
 public class Lab3Team3 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException, MalformedURLException {
-
-        BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
-        String key = "833921b016964f95905442e0fab0c229";
+        String key = "ERhdavZTGDwvkVEyiLtadECjGJbRdRdR";
 //        
         HttpURLConnection connection;
         URL url = new URL("https://www.ncdc.noaa.gov/cdo-web/api/v2/datacategories");
@@ -45,36 +39,33 @@ public class Lab3Team3 {
         try {
 
             connection.setRequestMethod("GET");
-            connection.setRequestProperty("Content-Type", "application/json");
             connection.setRequestProperty("Accept", "application/json");
-            connection.setRequestProperty("token", "ERhdavZTGDwvkVEyiLtadECjGJbRdRdR");
+            connection.setRequestProperty("token", key);
             connection.setInstanceFollowRedirects(false);
 
             HttpURLConnection.setFollowRedirects(true);
             connection.setDoOutput(true);
 
             InputStream ip = connection.getInputStream();
-            BufferedReader br
-                    = new BufferedReader(new InputStreamReader(ip));
+            BufferedReader br = new BufferedReader(new InputStreamReader(ip));
             StringBuilder sb = new StringBuilder();
             String line;
-            //This checks the API connection right away as per professor's tech tips example
-            //Not positive this is what he is asking for
             
-            if (connection.getResponseMessage() == null) {
-                System.out.println("Bad API Connection");
-            } else {
-                while ((line = br.readLine()) != null) {
+            while ((line = br.readLine()) != null) {
                     sb.append(line + "\n");
-                }
-                String xx = sb.toString();
-                br.close();
-                JsonParser jsonParser = new JsonParser();
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                JsonParser jp = new JsonParser();
-                JsonElement je = jp.parse(xx);
+            }
+            String xx = sb.toString();
+            br.close();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonParser jp = new JsonParser();
+            JsonElement je = jp.parse(xx);
+            JsonObject obj = je.getAsJsonObject();
+            // Check Json object per instructor's recommendation
+            if (obj.get("metadata") != null && obj.get("status") == null) {
                 String prettyJsonString = gson.toJson(je);
                 System.out.println(prettyJsonString);
+            } else {
+                System.out.println("No results found!");
             }
         } catch (MalformedURLException ex) {
             System.err.println("ERROR_MALFORMED_URL");
@@ -90,7 +81,6 @@ public class Lab3Team3 {
             //tech tips also showed the below as a method of closing both the socket and URL connection
             if (connection != null) {
                 connection.disconnect();
-                System.out.println("Disconnected");
             }
         }
     }
